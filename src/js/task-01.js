e44322d1ba29ebf3dae699d28b1e6c7fef02b9c1
Vitalii Poliakov <vitalik211287@gmail.com>
2022-08-13 12:00:03 +0300

    complited task 1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,26 +1,14 @@
+const categoriesListRef = document.querySelector('#categories')
+const categoriesItems = categoriesListRef.children
 
-const navEl = document.querySelectorAll(".item");
-console.log(`Number of categories: ${navEl.length}`); 
+// 1.
+const categoriesAmount = categoriesItems.length
+console.log(`Number of categories: ${categoriesAmount}`);
 
-const navElItems = document.querySelectorAll("h2");
-// const navElTitle = navElItems.forEach(
-//     (navElItems) => (navElItems = console.log(`Category: ${navElItems.textContent}`)));
-  
-const navElList = document.querySelectorAll("li.item > ul");
-// const navElList1 = navElList.forEach(
-//   (navElList) =>
-//         (navElList = console.log(`Elements: ${navElList.children.length}`))
-// );
-// const animal = {
-//     category() {`Elements: ${navElList[0].children.length} `},
-//     elItems() {`Category: ${navElItems[0].textContent}`},
-// };
-// console.log(animal.category);
-console.log(`Category: ${navElItems[0].textContent}`);
-console.log(`Elements: ${navElList[0].children.length}`);
-
-console.log(`Category: ${navElItems[1].textContent}`);
-console.log(`Elements: ${navElList[1].children.length}`);
-
-console.log(`Category: ${navElItems[2].textContent}`);
-console.log(`Elements: ${navElList[2].children.length}`);
+// 2.
+[...categoriesItems].forEach((item)=>{
+  const titleRef = item.querySelector("h2")
+  console.log(`Category: ${titleRef.textContent}`);
+  const items = item.querySelectorAll("li")
+  console.log(`Elements: ${items.length}`);
+})
