9419ec11e784cbede974b46dcb1a9db9ae2c3076
vitalik poliakov <vitalik211287@gmail.com>
2022-08-22 15:42:56 +0300

    Complite task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,11 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const bodyEl = document.querySelector("body");
+const buttonEl = document.querySelector(".change-color");
+const spanEl = document.querySelector(".color");
+
+buttonEl.addEventListener("click", (event) => {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  console.log(spanEl.textContent = bodyEl.getAttribute("style"));
+});
