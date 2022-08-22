a2c25ec06dc0a5bd43264f58805ba347e8594eef
vitalik poliakov <vitalik211287@gmail.com>
2022-08-22 14:53:32 +0300

    Complete task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,16 @@
+const formEl = document.querySelector(".login-form");
+console.dir(formEl);
 
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = formEl;
+
+  if (email.value === "" || password.value === "") {
+    return console.log("Please fill in all the fields!");
+  }
+  console.log(`Email: ${email.value}, Password: ${password.value}`);
+  formEl.reset();
+  console.log(formEl.value === formEl.value);
+});
